ece1c6690e6d7877f534db0c1592210359a6c24e
Bobby Johnson <bobby@madnesslabs.net>
2021-12-20 02:10:20 -0600

    fix(): Add presets to test select

---
+++ src/components/select/select.presets.ts
@@ -0,0 +1,54 @@
+const options = [
+  {
+    label: "Imortant",
+    value: "important",
+  },
+  {
+    label: "Bug",
+    value: "bug",
+  },
+  {
+    label: "Idea",
+    value: "idea",
+  },
+];
+
+export default {
+  default: {
+    name: "With Options",
+    props: {
+      label: "Tags",
+      multiple: true,
+      options,
+    },
+  },
+  withValue: {
+    name: "With Value",
+    props: {
+      label: "Tags",
+      options,
+      multiple: true,
+      value: ["important"],
+    },
+  },
+  withAdding: {
+    name: "With Adding",
+    props: {
+      allowAdding: true,
+      label: "Tags",
+      options,
+      multiple: true,
+      value: ["important"],
+    },
+  },
+  withEndpoint: {
+    name: "Data from API",
+    props: {
+      allowAdding: true,
+      label: "Tags",
+      endpoint: "listRoles",
+      resultsKey: "roles",
+      limit: 100,
+    },
+  },
+};
