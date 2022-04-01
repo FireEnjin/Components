3444d07c8a44668342a6c959977f0174fe4478c8
Popcorn245 <bobby@madnesslabs.net>
2022-03-31 21:16:41 -0500

    fix(): Setup presets to test input

--- src/components/input/input.presets.ts
+++ src/components/input/input.presets.ts
@@ -4,6 +4,20 @@ export default {
   default: {
     name: "Default",
     props: {
+      type: "text",
+      name: "test",
+    },
+  },
+  phone: {
+    name: "Phone",
+    props: {
+      type: "phone",
+      name: "phone",
+    },
+  },
+  card: {
+    name: "Card",
+    props: {
       type: "card",
       stripeKey: "pk_test_G6ksY0dKXlgogvnitD0Wm1oc",
     },
