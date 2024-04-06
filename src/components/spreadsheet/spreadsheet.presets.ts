cd033a3180328356a17abfa2938bcb4ac9a759e8
Popcorn245 <bobby@madnesslabs.net>
2024-04-06 01:09:47 -0500

    fix(): Update dependencies

---
+++ src/components/spreadsheet/spreadsheet.presets.ts
@@ -0,0 +1,17 @@
+export default {
+  default: {
+    props: {
+      columns: [
+        { data: "id", title: "ID" },
+        { data: "name", title: "Name" },
+      ],
+      data: [
+        { id: 1, name: "Ted Right" },
+        { id: 2, name: "Frank Honest" },
+        { id: 3, name: "Joan Well" },
+        { id: 4, name: "Gail Polite" },
+        { id: 5, name: "Michael Fair" },
+      ],
+    },
+  },
+};
